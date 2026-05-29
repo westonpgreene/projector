a08d2a0146574116711196616d291780ea3cc5c7
westonpgreene <westonpgreene@gmail.com>
2026-05-29 18:36:56 -0400

    Implementing initial project files

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type ProjectType int
+type Status int
+
+const (
+	Pending    Status = iota
+	InProgress        = 1
+	Completed         = 2
+)
+
+type Order struct {
+	ID           uuid.UUID
+	clientName   string
+	projectType  ProjectType
+	status       Status
+	deliveryDate time.Time
+}
+
+func createOrder(ctx *gin.Context) {
+
+}
+
+func retrieveOrder(ctx *gin.Context) {
+
+}
+
+func updateOrder(ctx *gin.Context) {
+
+}
+
+func deleteOrder(ctx *gin.Context) {
+
+}
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/orders", createOrder)
+	router.GET("/orders", retrieveOrder)
+	router.PUT("/orders", updateOrder)
+	router.DELETE("/orders", deleteOrder)
+}
