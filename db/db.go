cd5b24c871ed67144be364b56d0afe70e3bb253e
westonpgreene <westonpgreene@gmail.com>
2026-05-29 19:17:48 -0400

    Implementing SQLite database driver

---
+++ db/db.go
@@ -0,0 +1,19 @@
+package db
+
+import (
+    "src/projector/models"
+    "gorm.io/driver/sqlite"
+    "gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Init() error {
+    var err error
+    DB, err = gorm.Open(sqlite.Open("orders.db"), &gorm.Config{})
+    if err != nil {
+        return err
+    }
+    return DB.AutoMigrate(&models.Order{})
+}
+
